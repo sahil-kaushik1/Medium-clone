682b18082533e6027bc88d38d5277ebbe1bd2f75
Sahil Kaushik <sahilkaushik2444@gmail.com>
2024-08-28 21:54:19 +0530

    change

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -1,19 +1,37 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export  const   Appbar=()=>{
-const navigate=useNavigate();
-    return(
+export const Appbar = () => {
+    const navigate = useNavigate();
+    const [button, setButton] = useState("/publish");
+    const [route, setRoute] = useState(localStorage.getItem("token"));
 
-<div className="border-b-2 mb-10">
-    <div className="flex justify-between p-5">
-        <div className="text-2xl font-extrabold">
-        Medium
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        setRoute(token);
+
+        if (!token) {
+            setButton("/signup");
+        } else {
+            setButton("/publish");
+        }
+    }, []); 
+
+    return (
+        <div className="border-b-2 mb-10">
+            <div className="flex justify-between p-5">
+                <div className="text-2xl font-extrabold">
+                    Medium
+                </div>
+                <div>
+                    <button 
+                        className="bg-blue-400 rounded-lg p-2 border-2 border-slate-900" 
+                        onClick={() => navigate(button)}
+                    > 
+                        Publish a Blog
+                    </button>
+                </div>
+            </div>
         </div>
-        <div>
-            <button className="bg-blue-400 rounded-lg p-2 border-2 border-slate-900 " onClick={()=>navigate("/publish")}> Publish a blog</button>
-        </div>
-    </div>
-
-</div>
-)
-}
+    );
+};
